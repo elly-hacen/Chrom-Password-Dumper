6e92f51dc8d11867a3944b51176aa27f8a44c6e7
Ali Hassan <elly.hacen@gmail.com>
2023-05-12 21:27:49 +0500

    Initial commit: added main Python script and README file

---
+++ README.md
@@ -0,0 +1,21 @@
+# Passwords Extraction Tool for Google Chrome 🔑
+
+This Python script is a tool that can be used to extract saved login credentials from Google Chrome's SQLite database file and decrypt the encrypted passwords using the user's encryption key.
+
+## Prerequisites
+
+- Python 3.x
+- Required Python module: `pycryptodome==3.17`
+
+You can install the module using pip: `pip install pycryptodome==3.17`
+
+## Usage
+
+1. Download the repo `https://github.com/elly-hacen/chrome-password-dumper.git`
+2. Run the script in a Python environment (e.g. command prompt, terminal) using the command: `python chrome.py`
+3. The script will generate a file named `temp.txt` in the same directory where the program executed.
+4. The `temp.txt` file will contain the origin URL, username, password, and creation date for each saved login credential.
+
+## Important Note 🚨
+
+Using this tool to access other people's saved login credentials without their consent is unethical and potentially illegal. This tool should only be used for password management and recovery purposes for the user's own saved login credentials.
---
+++ chrome.py
@@ -0,0 +1,67 @@
+import os
+import json
+import shutil
+import base64
+import sqlite3
+import win32crypt
+from Crypto.Cipher import AES
+from datetime import datetime, timedelta
+
+
+
+def get_chrome_datetime(chromedate):
+    """Return a `datetime.datetime` object from a chrome format datetime
+    Since `chromedate` is formatted as the number of microseconds since January 1601"""
+    return datetime(1601, 1, 1) + timedelta(microseconds=chromedate)
+
+
+def get_encryption_key():
+    local_state_path = os.path.join(os.environ["USERPROFILE"],
+                                    "AppData", "Local", "Google", "Chrome",
+                                    "User Data", "Local State")
+    with open(local_state_path, "r", encoding="utf-8") as f:
+        local_state = f.read()
+        local_state = json.loads(local_state)
+
+    key = base64.b64decode(local_state["os_crypt"]["encrypted_key"])
+    key = key[5:]
+    return win32crypt.CryptUnprotectData(key, None, None, None, 0)[1]
+
+
+def decrypt_password(password, key):
+    try:
+        iv = password[3:15]
+        password = password[15:]
+        cipher = AES.new(key, AES.MODE_GCM, iv)
+        return cipher.decrypt(password)[:-16].decode()
+    except:
+        try:
+            return str(win32crypt.CryptUnprotectData(password, None, None, None, 0)[1])
+        except:
+            return ""
+
+
+def main():
+    key = get_encryption_key()
+    db_path = os.path.join(os.environ["USERPROFILE"], "AppData", "Local",
+                           "Google", "Chrome", "User Data", "default", "Login Data")
+    filename = "temp.db"
+    shutil.copyfile(db_path, filename)
+    
+    with sqlite3.connect(filename) as db, open('temp.txt', 'w') as f:
+        cursor = db.cursor()
+        cursor.execute("SELECT origin_url, username_value, password_value, date_created FROM logins")
+        for row in cursor.fetchall():
+            origin_url, username_value, password_value, date_created = row
+            password = decrypt_password(password_value, key)
+            if password:
+                date_created = get_chrome_datetime(date_created / 10**6)
+                f.write(f"Origin URL: {origin_url}\n")
+                f.write(f"Username: {username_value}\n")
+                f.write(f"Password: {password}\n")
+                f.write(f"Created: {date_created}\n\n")
+
+
+
+
+main()
